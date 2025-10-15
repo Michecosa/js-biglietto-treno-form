f196fb6435b9701fb02fa20d562589b25ecb0395
Michela <micheladellagatta1@gmail.com>
2025-10-15 14:06:10 +0200

    Get price element by id and display in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const ageIn = document.getElementById('age');
 const kmIn = document.getElementById('num-km');
+const priceOut = document.getElementById('price');
 
 console.log(ageIn);
 console.log(kmIn);
+console.log(priceOut);
