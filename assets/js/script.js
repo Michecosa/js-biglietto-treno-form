89247815997adfec09a7cf75db324ba9a00da15f
Michela <micheladellagatta1@gmail.com>
2025-10-15 14:06:59 +0200

    Get button element with query selector and display in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const ageIn = document.getElementById('age');
 const kmIn = document.getElementById('num-km');
 const priceOut = document.getElementById('price');
 
+const button = document.querySelector('button')
+
 console.log(ageIn);
 console.log(kmIn);
 console.log(priceOut);
+console.log(button);
