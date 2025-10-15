5b22da0f1e7e2052d146fd4163e9324734738c7b
Michela <micheladellagatta1@gmail.com>
2025-10-15 15:49:39 +0200

    Add new parameter to save offer name

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,10 +24,13 @@ const discountSeniors = 0.4;
 function trainPrice(age, km) {
   const priceNoDiscount = km * priceTimesKm;
   let discount = 0;
+  let offerName = 'Biglietto Standard';
   if(age < 18) {
     discount = discountMinors;
+    offerName = 'Sconto Minorenni (20%)';
   } else if(age > 65) {
     discount = discountSeniors;
+    offerName = 'Sconto Over 65 (40%)';
   }
   const priceFinal = (priceNoDiscount - priceNoDiscount * discount);
   return priceFinal;
